aa8f5c51a953d03fda5957c086d0ebe89d57f493
dsfsandeep9 <dsf.sandeep@gmail.com>
2021-05-13 18:03:24 +0530

    base_commit

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpcid" {
+  description = "List of IDs of instances"
+  value       = ["${aws_vpc.default.id}"]
+}
+
+output "vpcname" {
+  description = "List of IDs of instances"
+  value       = ["${aws_vpc.default.tags.Name}"]
+}
